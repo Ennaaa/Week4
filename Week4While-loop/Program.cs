cedc23fcdfb3e8cc1236c98947e729299ab3ad64
Anna D <denysenko.niura@gmail.com>
2023-05-07 12:55:58 +0300

    Week4

---
+++ Week4While-loop/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Random rmd = new Random();
+int cpuRandom = rmd.Next(1, 4);
+
+bool loopActive = true;
+
+int numberoftries = 0;
+
+while (numberoftries < 3)
+{
+    Console.WriteLine("Try to guess the cpu number:");
+    int userGues = Convert.ToInt32(Console.ReadLine());
+    numberoftries++;
+    if (userGues == cpuRandom)
+    {
+        Console.WriteLine("You won!");
+        break;
+    }
+}
+Console.WriteLine($"The number of tries: {numberoftries}");
+Console.WriteLine("Have a nice day!");
